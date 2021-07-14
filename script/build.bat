fd446a4d8c966d708717ef6ce54c7325a2d70499
Nelaturi Aiswarya <nelaturi.aiswarya@continental.com>
2021-07-14 18:21:00 +0530

    updated changes

--- script/build.bat
+++ script/build.bat
@@ -1,9 +1,10 @@
 @echo off
 echo "Cleaning the old executable .... "
 IF EXIST ..\src\output.exe DEL /F ..\src\output.exe | echo " Found and Deleted....."
+IF EXIST ..\reports\Report.html DEL /F ..\reports\Report.html | echo " Found report.html and Deleted....."
 set MINGW_PATH=%~dp0..\tools\strawberry32\c\bin
 set PATH=%MINGW_PATH%;%PATH%
 echo "Building the Application...."
 CALL %MINGW_PATH%\g++ ..\src\driver.cpp -o ..\src\output
-::CALL ..\src\output
+CALL ..\src\output
 pause
--- src/driver.cpp
+++ src/driver.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <fstream>
+using namespace std;
 class Numbers
 {
 private:
@@ -23,13 +25,32 @@ public:
 	{
 		return a * b;
 	}
+
+	void Generateresult(int res, int input1, int input2)
+	{
+		  ofstream myfile("D://Aish//demoProject//reports//Report.html" , ios::app);
+		  myfile << "<!DOCTYPE html><html><head><p>Test Results:</p></head><body>"; //starting html
+		  
+		 //add some html content
+		 myfile << "Input 1 = "<<input1 << " :  Input2 : "<< input2 << " => Result = "<<res;  
+		 
+		  
+		  
+		//ending html
+		myfile << "</body></html>";
+		myfile.close();
+	}
 };
 
-using namespace std;
+
 
 int main()
 {
-	Numbers obj(5, 6);
-	cout <<" the result is = "<< obj.Sum() << endl;
+	int input1 = 5, input2 =6;
+	Numbers obj(input1, input2);
+	obj.Generateresult(obj.Sum(),input1,input2);
+	obj.Generateresult(obj.difference(),input1,input2);
+	obj.Generateresult(obj.Multiplication(),input1,input2);
+	//cout <<"output is generated at this path: D:\Aish\demoProject\result\Report.html" << endl;
 	return 0;
 }
